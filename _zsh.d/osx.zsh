e468c26c4b1c92b7d758c876d62ae1fd8f6757e3
Naoto Yokoyama <builtinnya@gmail.com>
2014-03-02 14:45:21 +0900

    Add and fix some aliases

---
+++ _zsh.d/osx.zsh
@@ -0,0 +1,16 @@
+# ======================================================================
+# Zsh OSX-specific settings
+# ======================================================================
+
+# ======================================================================
+# Aliases
+# ======================================================================
+
+# Homebrew
+alias b="brew"
+alias bu="brew update"
+alias bug="brew update && brew upgrade"
+alias bi="brew install"
+alias br="brew uninstall"
+alias bs="brew search"
+alias bf="brew info"
