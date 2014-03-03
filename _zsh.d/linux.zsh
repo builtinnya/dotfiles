073fd4670f21c79e4c854165787433b5dadc16a0
Naoto Yokoyama <builtinnya@gmail.com>
2014-03-03 10:23:09 +0900

    Add aliases for aptitude

--- _zsh.d/linux.zsh
+++ _zsh.d/linux.zsh
@@ -5,3 +5,8 @@
 # ======================================================================
 # Aliases
 # ======================================================================
+apt="aptitude"
+au="sudo aptitude update"
+aug="sudo aptitude update && sudo aptitude upgrade"
+ai="sudo aptitude install"
+af="sudo aptitude update && aptitude search"
