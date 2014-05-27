5b67693312cc2d06236ea7150f23e223765528ae
Naoto Yokoyama <builtinnya@gmail.com>
2014-05-27 21:22:48 +0900

    Add _lein/profiles.clj

---
+++ _lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:dependencies [[slamhound "RELEASE"]
+                       [org.clojure/tools.trace "RELEASE"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :plugins [[lein-kibit "RELEASE"]
+                  [lein-immutant "1.2.1"]]}}
